fee67905418943dc3d7c2312f0e6a27e732cd4cb
Sharmeen <32082105+bengalspice@users.noreply.github.com>
2018-03-15 13:47:10 -0400

    Sumo activity

---
+++ sumo.ino
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeMCore.h>
+
+MeDCMotor motor_9(9);
+MeDCMotor motor_10(10);
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+          leftSpeed = speed;
+          rightSpeed = speed;
+      }else if(direction == 2){
+          leftSpeed = -speed;
+          rightSpeed = -speed;
+      }else if(direction == 3){
+          leftSpeed = -speed;
+          rightSpeed = speed;
+      }else if(direction == 4){
+          leftSpeed = speed;
+          rightSpeed = -speed;
+      }
+      motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
+      motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
+}
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+MeLineFollower linefollower_2(2);
+MeUltrasonicSensor ultrasonic_3(3);
+
+void setup(){
+}
+
+void loop(){
+    while(!((linefollower_2.readSensors()) < (3)))
+    {
+        _loop();
+        if((ultrasonic_3.distanceCm()) < (12)){
+            move(1,255);
+        }else{
+            move(1,100);
+        }
+        move(2,100);
+        _delay(0.5);
+        move(4,100);
+        _delay(0.5);
+    }
+    _loop();
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+}
+
